de8479ff298df83e6b00219b814a983fda30585e
Ivan <ivan.emidio@live.com.pt>
2024-05-18 14:38:13 +0100

    Creating a board to Dungeon Crawlers in Room.cs

--- YetAnotherDungeonCrawler/Program.cs
+++ YetAnotherDungeonCrawler/Program.cs
@@ -2,11 +2,23 @@
 
 namespace YetAnotherDungeonCrawler
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+        public static void Main()
+    
+        // criar board
+        Board board = new Board();
+
+        // Atribuir valores
+        board.SetValue(0, 0, 1);
+        board.SetValue(1, 1, 2);
+        board.SetValue(2, 2, 3);
+
+        // Print board
+        board.PrintBoard();
+    
         }
     }
 }
--- YetAnotherDungeonCrawler/Room.cs
+++ YetAnotherDungeonCrawler/Room.cs
@@ -7,6 +7,59 @@ namespace YetAnotherDungeonCrawler
 {
     public class Room
     {
-        
+    private int[,] board;
+
+    public Board()
+    {
+        // Inicializar um board 3x3
+        board = new int[3, 3];
+    }
+
+    // Atribuir Valores ao Board
+    public void SetValue(int row, int col, int value)
+    {
+        if (IsValidPosition(row, col))
+        {
+            board[row, col] = value;
+        }
+        else
+        {
+            Console.WriteLine("Invalid position!");
+        }
+    }
+
+    // Receber um Valor do Board
+    public int GetValue(int row, int col)
+    {
+        if (IsValidPosition(row, col))
+        {
+            return board[row, col];
+        }
+        else
+        {
+            Console.WriteLine("Invalid position!");
+            return -1; // Receber uma exceçao
+        }
+    }
+
+    // Print Board
+    public void PrintBoard()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                Console.Write(board[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    // Metodo para ver se uma posiçao e valida
+    private bool IsValidPosition(int row, int col)
+    {
+        return row >= 0 && row < 3 && col >= 0 && col < 3;
+    }
+
     }
 }
