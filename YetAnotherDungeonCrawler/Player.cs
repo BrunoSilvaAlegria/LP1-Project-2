ace01656a5b21a5ee423f5160a039aa1bcff360b
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-05-20 20:24:30 +0100

    Add player's name property, Health and AttackPower variables, and Player constructor

--- YetAnotherDungeonCrawler/Player.cs
+++ YetAnotherDungeonCrawler/Player.cs
@@ -7,6 +7,13 @@ namespace YetAnotherDungeonCrawler
 {
     public class Player
     {
-        
+        public string Name { get; } //Name entered by player
+        public int Health = 10; //Player's base life
+        private int AttackPower = 2; //Player's base attack power
+
+        public Player(string name)
+        {
+            Name = name;
+        }
     }
 }
