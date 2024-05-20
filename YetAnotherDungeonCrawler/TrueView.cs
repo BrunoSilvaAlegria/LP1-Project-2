c0390863fcac98fddd44c444c69ae4ec1cc7a6e7
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-05-20 20:28:08 +0100

    Add Controller variable and TrueView constructor, MainMenu and PlayerDetails methods detailed

--- YetAnotherDungeonCrawler/TrueView.cs
+++ YetAnotherDungeonCrawler/TrueView.cs
@@ -7,6 +7,31 @@ namespace YetAnotherDungeonCrawler
 {
     public class TrueView : IView
     {
-        
+        private readonly Controller controller;
+
+        public TrueView(Controller controller)
+        {
+            this.controller = controller;
+        }
+
+        public string MainMenu()
+        {            
+            Console.WriteLine("\ud83d\udde1\ufe0f Welcome adventurer! \ud83e\uddd9");
+
+            return "Hi";
+        }
+
+        public Player PlayerDetails()
+        {
+            string name;
+
+            Console.WriteLine("What is your name?");
+            name = Console.ReadLine();
+
+            return new Player(name);
+        }
+    
+
+
     }
 }
