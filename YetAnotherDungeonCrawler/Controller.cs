3187a3ce9b54616bd711b62a534493ae8d5de6f9
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-05-21 18:05:08 +0100

    Add Controller constructor, Start method, IView variable and item list

--- YetAnotherDungeonCrawler/Controller.cs
+++ YetAnotherDungeonCrawler/Controller.cs
@@ -7,6 +7,25 @@ namespace YetAnotherDungeonCrawler
 {
     public class Controller
     {
-        
+        private readonly List<Item> itemList;
+
+        private IView view;
+
+        public Controller(List<Item> items)
+        {
+            itemList = items;
+        }
+
+        public void Start(IView view)
+        {
+            this.view = view;
+
+            string option;
+
+            do
+            {
+                option = view.MainMenu();
+            } while (option == "North"||option == "South"||option == "East"||option == "West")
+        }
     }
 }
