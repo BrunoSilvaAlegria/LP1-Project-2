9b2c9a1812f17cd8e275661fd7ad7722e7ac476a
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-05-21 18:01:01 +0100

    Add ToString method

--- YetAnotherDungeonCrawler/Player.cs
+++ YetAnotherDungeonCrawler/Player.cs
@@ -10,10 +10,16 @@ namespace YetAnotherDungeonCrawler
         public string Name { get; } //Name entered by player
         public int Health = 10; //Player's base life
         private int AttackPower = 2; //Player's base attack power
+        public List<Item> Inventory = new List<Item>[,];
 
         public Player(string name)
         {
             Name = name;
         }
+
+        public override string ToString()
+        {
+            return $"Name";
+        } 
     }
 }
