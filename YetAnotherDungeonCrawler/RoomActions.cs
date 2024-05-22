208b4f3d7e9cefa24b28c8494e8a59f7436bc3bd
Fábio Ribeiro <fabioribeiro.info@gmail.com>
2024-05-22 09:35:06 +0100

    Add documentation XML in enumeration RoomActions and add possible actions

--- YetAnotherDungeonCrawler/RoomActions.cs
+++ YetAnotherDungeonCrawler/RoomActions.cs
@@ -1,10 +1,18 @@
 namespace YetAnotherDungeonCrawler
 {
+    /// <summary>
+    /// Enumeration responsible for available actions
+    /// in each room
+    /// </summary> <summary>
+    /// 
+    /// </summary>
     public enum RoomActions
     {
         "North";
         "South";
         "East";
         "West";
+        //"Attack";
+        //"PickItem";
     }
 }
