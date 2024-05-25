e24bd9ca2799e6a3d49e0de5edcd96c4c398b399
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-05-25 15:22:59 +0100

    Add Enemy health and attack power

--- YetAnotherDungeonCrawler/Enemy.cs
+++ YetAnotherDungeonCrawler/Enemy.cs
@@ -7,6 +7,13 @@ namespace YetAnotherDungeonCrawler
 {
     public class Enemy
     {
-        
+        public int Health { get; set; }
+        public int AttackPower { get; set; }
+
+        public Enemy()
+        {
+            Health = 10; // Enemy health 
+            AttackPower = 3; // Enemy attack power 
+        }
     }
 }
