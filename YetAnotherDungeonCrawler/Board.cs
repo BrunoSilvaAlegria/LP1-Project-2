bb66537800748c028d94f16b759904cf73cc137b
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-05-25 15:31:10 +0100

    Improve Board functionality

--- YetAnotherDungeonCrawler/Board.cs
+++ YetAnotherDungeonCrawler/Board.cs
@@ -2,63 +2,52 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace YetAnotherDungeonCrawler
 {
     public class Board
     {
-        private int[,] board;
+        public Room[,] Rooms { get; private set; }
 
-    public Board()
-    {
-        // Inicializar um board 3x3
-        board = new int[3, 3];
-    }
+        public Board(string filePath)
+        {
+            Rooms = new Room[3, 3];
+            ReadBoardData(filePath);
+        }
 
-    // Atribuir Valores ao Board
-    public void SetValue(int row, int col, int value)
-    {
-        if (IsValidPosition(row, col))
+        private void ReadBoardData(string filePath)
         {
-            board[row, col] = value;
-        }
-        else
-        {
-            Console.WriteLine("Invalid position!");
-        }
-    }
-
-    // Receber um Valor do Board
-    public int GetValue(int row, int col)
-    {
-        if (IsValidPosition(row, col))
-        {
-            return board[row, col];
-        }
-        else
-        {
-            Console.WriteLine("Invalid position!");
-            return -1; // Receber uma exceçao
-        }
-    }
-
-    // Print Board
-    public void PrintBoard()
-    {
-        for (int i = 0; i < 3; i++)
-        {
-            for (int j = 0; j < 3; j++)
+            using (StreamReader sr = new StreamReader(filePath))
             {
-                Console.Write(board[i, j] + " ");
-            }
-            Console.WriteLine();
-        }
-    }
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        string[] parts = line.Split(',');
+                        if (parts.Length != 8)
+                        {
+                            //Console.WriteLine("Error: Line does not contain exactly 8 values.");
+                            continue;
+                    }
 
-    // Metodo para ver se uma posiçao e valida
-    private bool IsValidPosition(int row, int col)
-    {
-        return row >= 0 && row < 3 && col >= 0 && col < 3;
-    }
+                    int x = int.Parse(parts[0]);
+                    int y = int.Parse(parts[1]);
+                    bool northExit = parts[2] == "1";
+                    bool southExit = parts[3] == "1";
+                    bool eastExit = parts[4] == "1";
+                    bool westExit = parts[5] == "1";
+                    bool hasEnemy = parts[6] == "1";
+                    bool hasItem = parts[7] == "1";
+
+                    Item roomItem = null;
+                    if (hasItem)
+                    {
+                        roomItem = new Item("Health Potion", 10); // Fixed item for all rooms that contain an item
+                    }
+                    Rooms[x, y] = new Room(x, y, northExit, southExit, eastExit, westExit, hasEnemy, roomItem);
+                    Console.WriteLine($"Created room at ({x},{y}) with northExit: {northExit}, southExit: {southExit}, eastExit: {eastExit}, westExit: {westExit}, hasEnemy: {hasEnemy}, hasItem: {hasItem}");
+                }
+            }
+        }
     }
 }
