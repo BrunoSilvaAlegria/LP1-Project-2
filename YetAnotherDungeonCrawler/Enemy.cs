adb64aaab2e2a2049af6096ed288f0d643a8504f
Fabio Ribeiro <fabioribeiro.info@gmail.com>
2024-05-26 10:21:24 +0100

    Add documentation in class Enemy

--- YetAnotherDungeonCrawler/Enemy.cs
+++ YetAnotherDungeonCrawler/Enemy.cs
@@ -5,11 +5,17 @@ using System.Threading.Tasks;
 
 namespace YetAnotherDungeonCrawler
 {
+    /// <summary>
+    /// Class responsible for
+    /// creating Enemy
+    /// </summary>
     public class Enemy
     {
         public int Health { get; set; }
         public int AttackPower { get; set; }
-
+        /// <summary>
+        /// Set Enemy Health and Attack Power
+        /// </summary>
         public Enemy()
         {
             Health = 10; // Enemy health 
