dc08601f1c642b1be3a97823820e243eebe22448
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-05-26 01:22:30 +0100

    Add more methods

--- YetAnotherDungeonCrawler/IView.cs
+++ YetAnotherDungeonCrawler/IView.cs
@@ -7,8 +7,16 @@ namespace YetAnotherDungeonCrawler
 {
     public interface IView
     {
+        string InitialMessage();
         string MainMenu();
         Player PlayerDetails();
+        string PlayerAttack();
+        string EnemyAttack();
+        string PlayerDeath();
+        string EnemyDeath();
+        void EndMessage();
+        void Choice();
+        void InvalidAction();
         void DisplayMessage(string message);
         string GetUserInput();
     }
