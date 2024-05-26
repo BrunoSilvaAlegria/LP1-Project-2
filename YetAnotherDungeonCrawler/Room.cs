b83df634938adb74705c7c7bf11503ca30d88c7a
Fabio Ribeiro <fabioribeiro.info@gmail.com>
2024-05-26 10:34:39 +0100

    Add documentation in class Room

--- YetAnotherDungeonCrawler/Room.cs
+++ YetAnotherDungeonCrawler/Room.cs
@@ -5,6 +5,10 @@ using System.Threading.Tasks;
 
 namespace YetAnotherDungeonCrawler
 {
+    /// <summary>
+    /// Class responsible for creating each Room
+    /// with exits, enemy or item
+    /// </summary>
     public class Room
     {
         public int X { get; private set; }
@@ -16,26 +20,34 @@ namespace YetAnotherDungeonCrawler
         public bool HasEnemy { get; private set; }
         public Item RoomItem { get; private set; }
 
+        /// <summary>
+        /// Constructor responsible for giving position for
+        /// the room and checking exits, enemy and item  
+        /// </summary>
         public Room(int x, int y, bool northExit, bool southExit, bool eastExit, bool westExit, bool hasEnemy, Item roomItem)
         {
-            X = x; //Gets the X coordinate
-            Y = y; //Gets the Y coordinate
+            X = x;
+            Y = y;
 
-            //Checks for an exit in a chosen direction
             NorthExit = northExit;
             SouthExit = southExit;
             EastExit = eastExit;
             WestExit = westExit;
 
-            HasEnemy = hasEnemy; //Checks for the presence of an enemy
-            RoomItem = roomItem; //Checks for the presence of an item
+            HasEnemy = hasEnemy;
+            RoomItem = roomItem;
         }
-
+        /// <summary>
+        /// Method responsible for removing Enemy
+        /// </summary>
         public void RemoveEnemy()
         {
             HasEnemy = false;
         }
 
+        /// <summary>
+        /// Method responsible for removing Item
+        /// </summary>
         public void RemoveItem()
         {
             RoomItem = null;
